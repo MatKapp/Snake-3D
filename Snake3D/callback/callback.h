7363524b0ed11c8e1ddeaac75ffdb1e6aea4fac7
Admin <Admin>
2017-04-01 17:45:58 +0200

    Better callback handling

--- Snake3D/callback/callback.h
+++
@@ -1,8 +0,0 @@
-#ifndef callback_h
-#define callback_h
-#include "../GLFW/glfw3.h"
-#include <stdio.h>
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod);
-
-#endif
